502d36a303277bcf8f6875cd0a1268ca92dced7f
lysyuk <lysyuk.y@gmail.com>
2022-05-20 12:13:57 +0300

    refactor: rename set main node height func

--- src/js/components/mobile-menu.js
+++ src/js/components/mobile-menu.js
@@ -1,19 +1,29 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
-  window.addEventListener('resize', updateMobileMenuHeight);
+// UI
+const mainNode = document.querySelector('.js-main');
 
-  // Deferred initial run for get right main node client height
-  setTimeout(() => {
-    updateMobileMenuHeight();
-  }, 200);
 });
 
-function updateMobileMenuHeight() {
-  const mainNode = document.querySelector('.js-main');
+// Listeners
+window.addEventListener('resize', onWindowResize);
 
+// Deferred initial run for get right main node client height
+setTimeout(() => {
+  setMainNodeHeightCssVar();
+}, 200);
+
+// Helpers
+
+function onWindowResize() {
+  setMainNodeHeightCssVar();
+}
+
+
+function setMainNodeHeightCssVar() {
   document.documentElement.style.setProperty(
     '--main-node-height',
     `${mainNode.clientHeight}px`
   );
 }
+
