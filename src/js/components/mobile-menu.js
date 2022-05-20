3ca7f6377c3e26908f4f1543a5ae38df2d7fc8bb
lysyuk <lysyuk.y@gmail.com>
2022-05-20 12:28:46 +0300

    feat: add auto scroll to anchor after load

--- src/js/components/mobile-menu.js
+++ src/js/components/mobile-menu.js
@@ -2,9 +2,18 @@
 
 // UI
 const mainNode = document.querySelector('.js-main');
+const sectionNodes = mainNode.querySelectorAll('.js-section');
 const menuToggleNode = document.querySelector('.js-menu-toggle');
 const headerNavNode = document.querySelector('.js-header-nav');
 
+const navMap = {};
+sectionNodes.forEach((sectionNode) => {
+  const id = sectionNode.id;
+
+  navMap[id] = {
+    link: headerNavNode.querySelector(`[href='#${id}']`),
+    section: sectionNode,
+  };
 });
 
 // Listeners
@@ -15,6 +24,8 @@ headerNavNode.addEventListener('click', onHeaderNavClick);
 setTimeout(() => {
   setMainNodeHeightCssVar();
 }, 200);
+
+initialScroll();
 
 // Helpers
 
@@ -35,5 +46,16 @@ function setMainNodeHeightCssVar() {
     '--main-node-height',
     `${mainNode.clientHeight}px`
   );
+}
+
+function initialScroll() {
+  const idFromHash = location.hash.slice(1);
+
+  if (idFromHash && navMap[idFromHash]) {
+    setTimeout(() => {
+      mainNode.scrollTop =
+        navMap[idFromHash].section.offsetTop - mainNode.offsetTop;
+    }, 500);
+  }
 }
 
