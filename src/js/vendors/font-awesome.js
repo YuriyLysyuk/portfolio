7f5c854cb95997d84f507f7f26dcb80435d318c5
lysyuk <lysyuk.y@gmail.com>
2022-06-24 11:51:21 +0300

    change close icon to fontawesome

--- src/js/vendors/font-awesome.js
+++ src/js/vendors/font-awesome.js
@@ -4,6 +4,10 @@ import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { faTelegram } from '@fortawesome/free-brands-svg-icons/faTelegram';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin';
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
+import { faXmark } from '@fortawesome/free-solid-svg-icons/faXmark';
 
 library.add(faTelegram, faLinkedin, faGithub);
+const icons = [faTelegram, faLinkedin, faGithub, faXmark];
+
+library.add(icons);
 dom.watch();
