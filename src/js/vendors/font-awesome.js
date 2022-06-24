472149a541fc55cb44947ce7a5af7462349a11ac
lysyuk <lysyuk.y@gmail.com>
2022-06-24 13:10:26 +0300

    add code icon to opened tabs

--- src/js/vendors/font-awesome.js
+++ src/js/vendors/font-awesome.js
@@ -5,9 +5,9 @@ import { faTelegram } from '@fortawesome/free-brands-svg-icons/faTelegram';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin';
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 import { faXmark } from '@fortawesome/free-solid-svg-icons/faXmark';
+import { faCode } from '@fortawesome/free-solid-svg-icons/faCode';
 
-library.add(faTelegram, faLinkedin, faGithub);
-const icons = [faTelegram, faLinkedin, faGithub, faXmark];
+const icons = [faTelegram, faLinkedin, faGithub, faXmark, faCode];
 
 library.add(icons);
 dom.watch();
