2e6c233bea33b9e1d2c99d13d4731f088af4c23d
lysyuk <lysyuk.y@gmail.com>
2022-06-25 12:28:49 +0300

    add side menu to sidebar

--- src/js/vendors/font-awesome.js
+++ src/js/vendors/font-awesome.js
@@ -6,8 +6,20 @@ import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin';
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 import { faXmark } from '@fortawesome/free-solid-svg-icons/faXmark';
 import { faCode } from '@fortawesome/free-solid-svg-icons/faCode';
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons/faCaretRight';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope';
+import { faPhone } from '@fortawesome/free-solid-svg-icons/faPhone';
 
-const icons = [faTelegram, faLinkedin, faGithub, faXmark, faCode];
+const icons = [
+  faTelegram,
+  faLinkedin,
+  faGithub,
+  faXmark,
+  faCode,
+  faCaretRight,
+  faEnvelope,
+  faPhone,
+];
 
 library.add(icons);
 dom.watch();
