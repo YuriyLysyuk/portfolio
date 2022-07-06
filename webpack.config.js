810245491570a4eca55485eb94cf28ddf317fd22
lysyuk <lysyuk.y@gmail.com>
2022-07-06 10:07:34 +0300

    config: add detect env mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,54 @@
 const path = require('path');
+const PugPlugin = require('pug-plugin');
 
-module.exports = {
-  mode: 'development',
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
 
-  context: path.resolve(__dirname, 'src'),
+  return {
+    mode: isProd ? 'production' : 'development',
 
-  entry: './js/main.js',
+    context: path.resolve(__dirname, 'src'),
 
-  devServer: {
-    static: './dist',
-    hot: false,
-    liveReload: true,
-    port: 9000,
-  },
+    entry: './js/main.js',
 
-  devtool: 'source-map',
+    devServer: {
+      static: './dist',
+      hot: false,
+      liveReload: true,
+      port: 9000,
+    },
 
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: '[contenthash].bundle.js',
-    clean: true,
-    assetModuleFilename: 'assets/[contenthash][ext][query]',
-  },
+    devtool: 'source-map',
 
-  plugins: [
-  ],
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: '[contenthash].bundle.js',
+      clean: true,
+      assetModuleFilename: 'assets/[contenthash][ext][query]',
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: ['css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.svg$/i,
-        type: 'asset',
-        use: 'svgo-loader',
-      },
-    ],
-  },
+    plugins: [],
+
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: ['css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.svg$/i,
+          type: 'asset',
+          use: 'svgo-loader',
+        },
+      ],
+    },
+  };
 };
